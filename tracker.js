431df9dfb240dda3b076b606acbcd59926ed852e
Emmanuel Nii Oblitei Tetteh <megamindtetteh538@gmail.com>
2025-11-21 07:58:03 +0000

    Add files via upload

---
+++ tracker.js
@@ -0,0 +1,41 @@
+
+function addNumbers(aa, bb,cc){
+                return aa + bb+cc;
+            }
+            let res=addNumbers(200,1200,5000);
+            console.log("Price=" ,res);
+
+
+        function addNumbers(dd,ee,ff){
+                        return dd+ee+ff;
+        }
+        let sum= addNumbers(500,200,300);
+        console.log("Quantity=" ,sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// cells[cells.length-1].textContent =rowSum;
+// grandTotal += rowSum;
+// }
+//  document.getElementById("Totalprice").textContent=`Total: ${Total}`;
+// }
